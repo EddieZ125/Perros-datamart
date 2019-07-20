a0972f229c746a33ae535ef932241960bb7b52ae
EddieZ125 <eddiezambrano125@gmail.com>
2019-07-20 08:06:03 -0400

    First commit

---
+++ database/migrations/2019_07_19_211646_perro.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Perro extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perro', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('nombre');
+            $table->float('precio',10,2);
+            $table->date('fecha');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('perro');
+    }
+}
